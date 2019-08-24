f53a314b07c9d645730d14d72e1b8cf61571766a
Siddharth Singh <sidharth_0903@hotmail.com>
2019-08-24 16:36:07 +0530

    Added 2 new metrics to check
    Inconvenience matrix
    Percentage people dropped

--- ga.py
+++ ga.py
@@ -8,25 +8,34 @@ from tqdm import tqdm
 import json
 import sys
 from helpers.route import Route, Routes
-from helpers.utils import get_node_vals, get_nbrs, random_walk, add_weights, fitness, simulate_people, GA, CreateGraph
+from helpers.utils import (
+    get_node_vals,
+    get_nbrs,
+    random_walk,
+    add_weights,
+    fitness,
+    simulate_people,
+    GA,
+    CreateGraph,
+)
 
 if len(sys.argv) < 2:
-    print('Usage: python3 ga.py [prefix for in/out file]')
+    print("Usage: python3 ga.py [prefix for in/out file]")
     exit(0)
 
 pre = sys.argv[1]
 
 # Get the graph
-G = CreateGraph(75, fname=f'./data/{pre}_final_full_graph.csv', pre=pre, node_prob=True)
+G = CreateGraph(75, fname=f"./data/{pre}_final_full_graph.csv", pre=pre, node_prob=True)
 
 # Hyper parameters
 pop_size = 100
-walk_length = [7, 15]
+walk_length = [3, 15]
 num_routes = [6, 12]
 num_ppl = 50000
 cap = 60
 # c1: [0,1]*50, c2: [0,1]*10 c3: avg_len = 10.98
-c1,c2,c3 = (50,20,100)
+c1, c2, c3 = (50, 20, 100)
 opt_bus = 20
 elite = 0.1
 iter = 20
@@ -50,14 +59,33 @@ Route.initialize_class(G)
 Routes.initialize_class(G)
 
 # Use a GA to solve the problem
-best, final_pop = GA(iter, pop, pop_size, G, num_ppl, c1, c2, c3, opt_bus, elite, mutation_prob, crossover_perc)
+best, final_pop = GA(
+    iter,
+    pop,
+    pop_size,
+    G,
+    num_ppl,
+    c1,
+    c2,
+    c3,
+    opt_bus,
+    elite,
+    mutation_prob,
+    crossover_perc,
+)
 
-print('\nFinal Solution:')
+print("\nFinal Solution:")
 print(best)
-opt_seats_taken, opt_num_bus, opt_cum_len = fitness(best, ppl, c1, c2, c3, opt_bus, components=True)
-print(f'\nThis route has {opt_num_bus} buses, On average, {opt_seats_taken}% of seats are occupied, Total length of all routes combined is {round(opt_cum_len,2)} km')
+opt_seats_taken, opt_num_bus, opt_cum_len = fitness(
+    best, ppl, c1, c2, c3, opt_bus, components=True
+)
+print(
+    f"\nThis route has {opt_num_bus} buses, On average, {opt_seats_taken}% of seats are occupied, Total length of all routes combined is {round(opt_cum_len,2)} km"
+)
 
-data_out = { 'data': [(int(route.num), [int(r) for r in route.v]) for route in best.routes] }
+data_out = {
+    "data": [(int(route.num), [int(r) for r in route.v]) for route in best.routes]
+}
 
-with open('data/{pre}_optimal_routes.json','w') as f:
+with open("data/{pre}_optimal_routes.json", "w") as f:
     json.dump(data_out, f, indent=2)
